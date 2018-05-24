08e0ab44dd9cd01aa87f0cf76f5304cdc297cbcc
mkiterian <hmukite@gmail.com>
2018-05-24 10:46:59 +0300

    [Fix] env and redirect url

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const configs = {
   },
   staging: {
     AUTH_API: 'https://api.andela.com/login?redirect_url=',
-    APP_URL: 'https://societies-staging.andela.com',
-    API_BASE_URL: 'https://societies-api-staging.andela.com/api/v1',
+    APP_URL: 'https://staging-societies.andela.com',
+    API_BASE_URL: 'https://private-ae5c2-andelasocietiesapi.apiary-mock.com/api/v1',
   },
   development: {
     AUTH_API: 'https://api.andela.com/login?redirect_url=',
