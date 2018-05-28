07d627017e1e2ed1c0e1c72ee0fb0486dbb07d06
mkiterian <hmukite@gmail.com>
2018-05-28 12:23:15 +0300

    [Fix] jsdocs

--- src/common/TextArea.jsx
+++ src/common/TextArea.jsx
@@ -26,11 +26,10 @@ const TextArea = props => (
     * @name propTypes
     * @type {PropType}
     * @param {Object} propTypes - React PropTypes
-    * @property {String} name - The name of the textArea
-    *@property {String} title - The title of the label
-    *@property {String} name - The name for the textarea
-    *@property {String} resize - whether the textarea should be fixed or resizable
-    *@property {String} placehold - the placeholder text
+    * @property {String} title - The title of the label
+    * @property {String} name - The name for the textarea
+    * @property {String} resize - whether the textarea should be fixed or resizable
+    * @property {String} placehold - the placeholder text
   */
 TextArea.propTypes = {
   title: PropTypes.string.isRequired,
